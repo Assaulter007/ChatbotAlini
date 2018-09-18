95f2808e1338d8448198141d4ea09f7e94e38ec6
Diego Natalo <34862688+KernelDN@users.noreply.github.com>
2018-09-18 08:35:02 -0300

    Update templates and static folders

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
 function getBotResponse() {
     var rawText = $("#textInput").val();
-    var userHtml = '<div class="col-md-6 offset-md-5 msg-box-sended">'+rawText+'</div>';
+    var userHtml = '<div class="col-8 col-md-7 offset-md-4 offset-3 msg-box-sended">'+rawText+'</div>';
     $("#textInput").val("");
     $("#chat-container").append(userHtml);
     $("#chat-container").animate({ scrollTop: 1000000 }, 100);
     $.get("/get", { msg: rawText }).done(function(data) {
-        var botHtml = '<div class="col-md-6 offset-md-2 msg-box-received">'+ data +'</div>';
+        var botHtml = '<div class="col-8 col-md-7 offset-md-1 msg-box-received">'+ data +'</div>';
         $("#chat-container").append(botHtml);
     $("#chat-container").animate({ scrollTop: 1000000 }, 100);
     });
